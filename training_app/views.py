7ef82c25c6c5f50cb6e36a748bd3e03b977fc908
badri ramtel <ramtel.badri@gmail.com>
2023-05-04 22:32:35 +0545

    add creat course functionality

--- training_app/views.py
+++ training_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from training_app.models import UpcomingClass, Course
 
 # Create your views here.
@@ -11,4 +11,22 @@ def courses(request):
     return render(request, 'training_app/courses.html', {'courses' : courses})
 
 def add_course(request):
-    return render(request, 'training_app/add_course.html')
+    # print('*******')
+    # print(request.method)
+    # print(request.POST)
+    if request.method == 'GET':
+        return render(request, 'training_app/add_course.html')
+    
+    elif request.method == 'POST':
+        title = request.POST['title']
+        desc = request.POST['description']
+        duration = request.POST['duration']
+        price = request.POST['price']
+        level = request.POST['level']
+
+        # c = Course(title = title, description = desc, duration = duration, price = price, level = level)
+        # c.save()
+        Course.objects.create(title = title, description = desc, duration = duration, price = price, level = level)
+
+        # return render(request, 'training_app/courses.html') --> it just show the page of other html in its own page
+        return redirect('training-course')
